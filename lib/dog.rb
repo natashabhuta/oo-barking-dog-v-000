5921506973c3a9f92585dda6d87338a37749af12
natashabhuta <natasha.bhuta@gmail.com>
2018-08-03 03:28:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 # Your code goes here!
-class Dog(name)
-  def initialize(name)
-  @name = name 
-  end 
-  def name
-    "{name}".chomp
-  end
-end 
+
   
- 
+def Dog.bark 
+ puts "woof!"
+ end 
   
 
